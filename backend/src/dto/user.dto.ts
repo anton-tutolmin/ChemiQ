6c61e65ddf9b99f3a62feedf7d43efa85dc330d4
Anton Tutolmin <crysisage@gmail.com>
2020-10-18 09:27:13 +0600

    fix userDto

--- backend/src/dto/user.dto.ts
+++ backend/src/dto/user.dto.ts
@@ -1,12 +1,14 @@
 import { User } from '../models/user.model';
 
 export class UserDto {
+  private id: number;
   private username: string;
   private email: string;
   private totalAnswers: number;
   private rightAnswers: number;
 
   constructor(user: User) {
+    this.id = user.id;
     this.username = user.username;
     this.email = user.email;
     this.totalAnswers = user.totalAnswers;
